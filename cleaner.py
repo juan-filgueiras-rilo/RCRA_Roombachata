5b2067611e49830f469cb8b8ba74f62b4e43eb9b
xzach <edduardofe@hotmail.es>
2018-04-19 18:47:53 +0200

    anadido el podado de lineas con espacios y saltos de linea solo

--- cleaner.py
+++ cleaner.py
@@ -33,15 +33,14 @@ class tabla:
 		j=0
 		linea="Apruebanos"
 		while (linea!=""):
-			if (linea=="\n"): 
+			linea=f.readline()
+			if (linea.lstrip(' ').lstrip("\n")==""): 
 				linea=f.readline()
 				continue
-			self.maxx= len(linea)-2
-			
-			linea=f.readline()
-
-			for i in range(0,len(linea)-1):
-				self.lmapa.append((linea[i]))
+			else:
+				self.maxx= len(linea)-2
+				for i in range(0,len(linea)-1):
+					self.lmapa.append((linea[i]))
 	def toClingo(self,salida):
 		pass
 	def showMapa(self,salida):
@@ -49,6 +48,7 @@ class tabla:
 		for i in range(0,len(self.lmapa)):
 			if ((i % (self.maxx+1) == 0) and (i!=0)):
 				salida.storetail("\n")
+			#print(self.gettype(self.lmapa[i]))		
 			salida.storetail("tile("+str(math.floor(i/(self.maxx+1))+1)+","+str(i%(self.maxx+1)+1)+","+self.gettype(self.lmapa[i])+").\n")
 	def gettype (self,simbolo):
 		if simbolo=="#": return "obs"
